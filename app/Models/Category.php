9e4ce2316ffae42b27268e87d8cf99d9835ccfd5
AlvinSanudharma <alvinsanudharma@gmail.com>
2024-10-04 22:46:12 +0800

    Belajar ORM dan Fillable Model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'icon',
+        'slug'
+    ];
+
+    public function tours() {
+        return $this->hasMany(PackageTour::class);
+    }
 }
--- app/Models/PackageBooking.php
+++ app/Models/PackageBooking.php
@@ -4,8 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PackageBooking extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'package_tour_id',
+        'user_id',
+        'quantity',
+        'total_amount',
+        'insurance',
+        'tax',
+        'sub_total',
+        'start_date',
+        'end_date',
+        'is_paid',
+    ];
+
+    protected function casts() {
+        return [
+            'start_date' => 'date',
+            'end_date' => 'date',
+        ];
+    }
+
+    public function tour() {
+        return $this->belongsTo(PackageTour::class);
+    }
+
+    public function customer() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function bank() {
+        return $this->belongsTo(PackageBank::class);
+    }
 }
