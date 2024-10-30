5afc0de0230c3ee3a954a63df5d339c85df05740
AlvinSanudharma <alvinsanudharma@gmail.com>
2024-10-30 11:54:25 +0800

    Coding Fitur Tampilkan Data Tour

--- app/Http/Controllers/PackageTourController.php
+++ app/Http/Controllers/PackageTourController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PackageTourRequest;
+use App\Models\Category;
 use App\Models\PackageTour;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class PackageTourController extends Controller
 {
@@ -12,7 +17,9 @@ class PackageTourController extends Controller
      */
     public function index()
     {
-        //
+        $tours = PackageTour::orderByDesc('id')->paginate(10);
+
+        return view('admin.package_tours.index', compact('tours'));
     }
 
     /**
@@ -20,15 +27,41 @@ class PackageTourController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::orderByDesc('id')->get();
+        
+        return view('admin.package_tours.create', compact('categories'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(PackageTourRequest $request)
     {
-        //
+        DB::transaction(function() use ($request) {
+            $validated = $request->validated();
+            
+            if ($request->hasFile('thumbnail')) {
+                $thumbnailPath = $request->file('thumbnail')->store('thumbnails/' . date('Y/m/d'), 'public');
+    
+                $validated['thumbnail'] = Storage::url($thumbnailPath);
+            }
+
+            $validated['slug'] = Str::slug($validated['name']);
+
+            $packageTour = PackageTour::create($validated);
+
+            if ($request->hasFile('photos')) {
+                foreach ($request->file('photos') as $photo) {
+                    $photoPath = $photo->store('package_photos/' . date('Y/m/d'), 'public');
+
+                    $packageTour->package_photos()->create([
+                        'photo' => $photoPath
+                    ]);
+                }
+            }
+        });
+        
+        return redirect()->route('admin.package_tours.index');
     }
 
     /**
